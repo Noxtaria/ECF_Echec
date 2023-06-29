3de8360950657d3660dd21e4b85dcebdcf4c13be
Kévin BUANNEC <noxtariadev@gmail.com>
2023-06-29 08:45:05 +0200

    Init project

---
+++ src/main/java/com/example/ecf3echec/Ecf3EchecApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecf3echec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ecf3EchecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ecf3EchecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ecf3echec/Ecf3EchecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecf3echec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ecf3EchecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
